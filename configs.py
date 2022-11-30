4875ed925c695069340c405c00fc64d41e526b44
Petr Volnov <neafiol.io@gmail.com>
2022-11-29 20:12:06 -0800

    feat: sse events

--- configs.py
+++ configs.py
@@ -2,4 +2,4 @@ with open("apple-app-site-association", "r") as f:
     APPLE_APP_SITE_ASSOCIATION = f.read()
 
 with open("apy_key", "r") as f:
-    APY_KEY = f.read()
+    APY_KEY = f.read().replace("\n", "")
