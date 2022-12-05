4bfb51318b0875a907b53ea4851888bedc5ec687
Petr Volnov <neafiol.io@gmail.com>
2022-12-05 12:26:37 -0800

    feat: sse events

--- routes.py
+++ routes.py
@@ -134,4 +134,4 @@ async def message_stream(
 def redirect_to_web(
     request: str,
 ):
-    return RedirectResponse(url=f"https://web.herewallet.app/approve/{request}&new")
+    return RedirectResponse(url=f"https://web.herewallet.app/approve/{request}?new")
